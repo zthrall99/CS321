5a79ff1751827ba667134dcb390438717cf68d9c
zthrall99 <76174403+zthrall99@users.noreply.github.com>
2022-09-26 19:32:36 -0700

    started seconds.c from jiffies.c clone

---
+++ HW1/Q.6/seconds.c
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * seconds.c                                                                 
+ * Name: Zach Thrall
+ *****************************************************************************/
+ 
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/jiffies.h>
+
+#define BUFFER_SIZE 128
+#define PROC_NAME "seconds"
+
+//Function prototypes
+static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+        .owner = THIS_MODULE,
+        .read = proc_read,
+};
+
+//This function is called when the module is loaded.
+static int proc_init(void)
+{
+	proc_create(PROC_NAME, 0, NULL, &proc_ops);
+	printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+	
+       	return 0;
+}
+
+//This function is called when the module is removed.
+static void proc_exit(void) 
+{
+	remove_proc_entry(PROC_NAME, NULL);
+
+        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+
+        if (completed) {
+                completed = 0;
+                return 0;
+        }
+
+        completed = 1;
+
+        rv = sprintf(buffer, "Jiffies Value: %lu\n", jiffies);
+
+        // copies the contents of buffer to userspace usr_buf
+        copy_to_user(usr_buf, buffer, rv);
+
+        return rv;
+}
+
+//Macros for registering module entry and exit points.
+module_init( proc_init ); 
+module_exit( proc_exit );
